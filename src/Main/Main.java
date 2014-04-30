757ad177641c0ff2a93f20194d1a8a0d53d7ed10
Hunter Lang <hunter@orbotix.com>
2014-04-30 05:35:19 -0600

    Main commented

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,20 +9,30 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Startup class for an instance of the game
+ */
 public class Main {
 
+    /**
+     * Window size used for sizing components
+     */
     public static final int WINDOW_SIZE = 800;
+
+    /**
+     * Communications port
+     */
     public static final int PORT = 51042;
-    private final String DEFAULT_NAME = "Tic-Tac-Toe Beginner";
+
 
     private String name;
-
     private JFrame mainFrame;
-    private JButton serverButton;
-    private JButton clientButton;
-
     private GamePlayer currentGame;
 
+    /**
+     * Default constructor
+     * Creates a new instance of the game
+     */
     public Main() {
 
         mainFrame = new JFrame("Tic-Tac-Toe");
@@ -30,7 +40,7 @@ public class Main {
         mainFrame.setLocation(50,50);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        serverButton = new JButton("Host a game");
+        JButton serverButton = new JButton("Host a game");
         serverButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -40,7 +50,7 @@ public class Main {
                 mainFrame.setVisible(false);
             }
         });
-        clientButton = new JButton("Join a game");
+        JButton clientButton = new JButton("Join a game");
         clientButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -59,11 +69,15 @@ public class Main {
 
         name = JOptionPane.showInputDialog( mainFrame, "Enter a name:", "Tic-Tac-Toe", JOptionPane.QUESTION_MESSAGE );
         if (name == null || name.isEmpty()) {
-            name = DEFAULT_NAME;
+            name = "Tic-Tac-Toe Beginner";
         }
         mainFrame.setTitle("Tic-Tac-Toe: Player " + name);
     }
 
+    /**
+     * Startup method for the application
+     * @param args          command line arguments
+     */
     public static void main(String[] args) {
         Main main = new Main();
     }
